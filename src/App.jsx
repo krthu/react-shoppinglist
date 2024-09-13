360127db54235f7952c7a35492e1b9848bc24bdd
KT <thun.kristian@gmail.com>
2024-09-13 15:33:40 +0200

    Can add more lists and save to local storage

--- src/App.jsx
+++ src/App.jsx
@@ -10,10 +10,10 @@ function App() {
   const [lists, setLists] = useState(null);
   const [currentListIndex, setCurrentListIndex] = useState(null)
 
-  const handelListChange = (newList) => {
+  const handelListChange = (newItems) => {
     const updatedLists = lists.map((list, i) => {
       if (i === currentListIndex) {
-        return newList;
+        return {...list, items: newItems};
       } else {
         return list;
       }
@@ -22,7 +22,10 @@ function App() {
   }
 
   const createNewList = () => {
-    const newList = []
+    const newList = {
+      name: `List ${lists.length + 1}`,
+      items: []
+    };
     setLists(prevLists => {
       const updatedLists = [...prevLists, newList];
       setCurrentListIndex(updatedLists.length - 1); // Sätter index till den nya listan
@@ -32,7 +35,7 @@ function App() {
 
 
   const addItemToList = (item) => {
-    const updatedList = [...lists[currentListIndex], item]
+    const updatedList = [...lists[currentListIndex].items, item]
     handelListChange(updatedList)
   }
 
@@ -40,22 +43,26 @@ function App() {
 
  
 
-  // useEffect(() => {
-  //   if (list !== null) {
-  //     localStorage.setItem('saved-items', JSON.stringify(list));
-  //     // console.log(`items saved -> ${list}`)
-  //   }
+  useEffect(() => {
+    if (lists !== null) {
+      localStorage.setItem('saved-items', JSON.stringify(lists));
+      // console.log(`items saved -> ${list}`)
+    }
 
-  // }, [list]);
+  }, [lists]);
 
    useEffect(() => {
      const savedItems = JSON.parse(localStorage.getItem('saved-items'));
-     // console.log(`items-loaded -> ${savedItems}`)
-     if (savedItems) {
+     console.log(`items-loaded -> ${savedItems}`)
+   
+     console.log(`Parsed items:`, savedItems);
+     if (savedItems && savedItems.length > 0) {
        setLists(savedItems);
      } else {
-       setLists([])
+      const defaultList = [{ name: 'list', items: [] }];
+       setLists(defaultList);
      }
+     setCurrentListIndex(savedItems.length - 1);
 
 
   }, [])
@@ -65,14 +72,20 @@ function App() {
 
         <>
           <header>
-          <h1>ShoppingList</h1>
-          <p onClick={createNewList}>Add list</p>
+            <h1>ShoppingList</h1>
+            <p onClick={createNewList}>Add list</p>
+            {currentListIndex !== null  && lists[currentListIndex] ?
+            
+            (<p>{lists[currentListIndex].name}</p>)
+            :
+            (<p></p>)
+            }
           </header>
 
           <main>
             {currentListIndex !== null  && lists[currentListIndex] ? (
               <List
-                list={lists[currentListIndex]}
+                list={lists[currentListIndex].items}
                 listChanged={handelListChange}
                 // toggleDone={toggleDone}
                 // deleteItemFromList={deleteItemFromList}
